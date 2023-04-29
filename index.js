47fc53585518c2734b52ae9154c2256e3fa8e421
red.jpo <127202284+red-jpo@users.noreply.github.com>
2023-04-29 09:04:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,26 +15,40 @@ document.addEventListener("click", (e) => {
   }
 });
 
-/* its the second time i run into the issue of the first click not returning result, first I went tried to think my way around it when I was not actually sure what was causing it, I was able to solve it, but running into the issue a second time made me think, if its a common problem then it must have a common solution, i was right it was simply a matter of where the js was looking for that style. the reason is pretty simple: when you get a "style" property, it represents inline styles of that element. On fist click there is no inline style for "display", so else fork fires and sets inline style for "display" to "none" by using computed style, like so,   const shareToDivStyle = window.getComputedStyle(shareToDiv); the js passou a incluir styles from all sources (inline, internal, external, etc.).
+const authorInfo = document.querySelector(".author-info");
+const authorShare = document.querySelector(".author-share");
+const shareDiv = document.querySelector(".toggle-share");
 
+function handleShareBtnClick() {
+  if (authorInfo.style.display !== "none") {
+    authorInfo.style.display = "none";
+    authorShare.style.backgroundColor = "hsl(217, 19%, 35%)";
+    shareDiv.style.display = "inline-flex";
+  } else {
+    authorInfo.style.display = "inline-flex";
+    authorShare.style.backgroundColor = "white";
+    shareDiv.style.display = "none";
+  }
+}
 
+shareBtn.addEventListener("click", handleShareBtnClick);
 
+function handleDocumentClick(event) {
+  if (!shareBtn.contains(event.target) && !shareDiv.contains(event.target)) {
+    authorInfo.style.display = "inline-flex";
+    authorShare.style.backgroundColor = "white";
+    shareDiv.style.display = "none";
+  }
+}
 
-Here's a more readable version of your text:
+document.addEventListener("click", handleDocumentClick);
 
-I encountered an issue where the first click wasn't returning a result. At first, I tried to solve the problem by thinking my way around it, but I wasn't actually sure what was causing it. I was able to fix it, but when I ran into the same issue a second time, I realized that if it's a common problem, there must be a common solution.
+function handleResize() {
+  if (window.innerWidth >= 768) {
+    authorInfo.style.display = "inline-flex";
+    authorShare.style.backgroundColor = "initial";
+    shareDiv.style.display = "none";
+  }
+}
 
-And I was right. It was simply a matter of where the JavaScript was looking for that style. The reason is pretty simple: when you get a "style" property, it represents the inline styles of that element. On the first click, there is no inline style for "display," so the else fork fires and sets the inline style for "display" to "none" by using computed style. I did this by including styles from all sources, such as inline, internal, external, etc., like this:
-
-```
-const shareToDivStyle = window.getComputedStyle(shareToDiv);
-```
-
-
-
-
-The issue was that on the first click, the JavaScript was not finding the inline style for the "display" property, which caused the code to execute the else branch and set the "display" property to "none". The solution was to use window.getComputedStyle() to get the styles from all sources, including inline, internal, and external stylesheets, and use that to set the "display" property. This ensures that the correct value for "display" is retrieved even if it is not defined inline.
-
-Overall, it seems like a reasonable solution to the problem. Using window.getComputedStyle() is a common approach to get the computed style of an element, and it ensures that you get the correct values for all the styles applied to the element.
-
-*/
+window.addEventListener("resize", handleResize);
